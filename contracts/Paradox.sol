69ef52980757b2919988671640dd1a4acf036a7b
k <mja>
2022-07-21 14:05:38 -0700

    paradox name change

--- contracts/Paradox.sol
+++ contracts/Paradox.sol
@@ -5,10 +5,10 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract ParadoxTokeneqe is ERC20, ERC20Burnable, AccessControl {
+contract ParadoxToken is ERC20, ERC20Burnable, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    constructor() ERC20("Paradox Tokeneqe", "PARA") {
+    constructor() ERC20("Paradox", "PARA") {
         _mint(msg.sender, 1000000000 * 10 ** decimals());
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
